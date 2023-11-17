d89fbce685b34cb7b40c8e70d2a81d6a6e0d9e01
id373 <id373@cam.ac.uk>
2023-11-17 13:57:43 +0000

    prices_coordinates_check

--- fynesse/access.py
+++ fynesse/access.py
@@ -65,6 +65,142 @@ def download_csv(url, filename, target_dir="", extract=False):
     except Exception as e:
         print(f"Error downloading file from: {url}\n{e}")
         
+def create_pp_data(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS pp_data;")
+        cursor.execute("""
+                    CREATE TABLE `pp_data` (
+                    `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
+                    `price` int(10) unsigned NOT NULL,
+                    `date_of_transfer` date NOT NULL,
+                    `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+                    `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                    `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+                    `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                    `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+                    `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+                    `street` tinytext COLLATE utf8_bin NOT NULL,
+                    `locality` tinytext COLLATE utf8_bin NOT NULL,
+                    `town_city` tinytext COLLATE utf8_bin NOT NULL,
+                    `district` tinytext COLLATE utf8_bin NOT NULL,
+                    `county` tinytext COLLATE utf8_bin NOT NULL,
+                    `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
+                    `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+                    `db_id` bigint(20) unsigned NOT NULL
+                    ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+                    """)
+        cursor.execute("""
+                    ALTER TABLE pp_data
+                    ADD PRIMARY KEY (`db_id`);
+                    """)
+        cursor.execute("""
+                    ALTER TABLE pp_data
+                    MODIFY db_id bigint(20) unsigned NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+                    """)
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        print(f"Error creating pp_data table: {e}")
+        
+def create_postcode_data(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS postcode_data;")
+        cursor.execute("""
+                        CREATE TABLE `postcode_data` (
+                        `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+                        `status` enum('live','terminated') NOT NULL,
+                        `usertype` enum('small', 'large') NOT NULL,
+                        `easting` int unsigned,
+                        `northing` int unsigned,
+                        `positional_quality_indicator` int NOT NULL,
+                        `country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,
+                        `latitude` decimal(11,8) NOT NULL,
+                        `longitude` decimal(10,8) NOT NULL,
+                        `postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,
+                        `postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,
+                        `postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,
+                        `postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,
+                        `postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,
+                        `postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,
+                        `outcode` varchar(4) COLLATE utf8_bin NOT NULL,
+                        `incode` varchar(3)  COLLATE utf8_bin NOT NULL,
+                        `db_id` bigint(20) unsigned NOT NULL
+                        ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+                    """)
+        cursor.execute("""
+                    ALTER TABLE postcode_data
+                    ADD PRIMARY KEY (`db_id`);
+                    """)
+        cursor.execute("""
+                    ALTER TABLE postcode_data
+                    MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+                    """)
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        print(f"Error creating postcode_data table: {e}")
+    
+def create_prices_coordinates_data(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS prices_coordinates_data;")
+        cursor.execute("""
+                        CREATE TABLE `prices_coordinates_data` (
+                        `price` int(10) unsigned NOT NULL,
+                        `date_of_transfer` date NOT NULL,
+                        `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+                        `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                        `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+                        `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+                        `locality` tinytext COLLATE utf8_bin NOT NULL,
+                        `town_city` tinytext COLLATE utf8_bin NOT NULL,
+                        `district` tinytext COLLATE utf8_bin NOT NULL,
+                        `county` tinytext COLLATE utf8_bin NOT NULL,
+                        `country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,
+                        `latitude` decimal(11,8) NOT NULL,
+                        `longitude` decimal(10,8) NOT NULL,
+                        `db_id` bigint(20) unsigned NOT NULL
+                    ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+                    """)
+        cursor.execute("""
+                    ALTER TABLE prices_coordinates_data
+                    ADD PRIMARY KEY (`db_id`);
+                    """)
+        cursor.execute("""
+                    ALTER TABLE prices_coordinates_data
+                    MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+                    """)
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        print(f"Error creating prices_coordinates_data table: {e}")
+        
+def index_pp_data(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+                    CREATE INDEX index_postcode ON pp_data(postcode);
+                    """)
+        cursor.execute("""
+                    CREATE INDEX index_date ON pp_data(date_of_transfer);
+                    """)
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        print(f"Error indexing pp_data table: {e}")
+        
+def create_index(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+                    CREATE INDEX index_postcode ON postcode_data(postcode);
+                    """)
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        print(f"Error indexing postcode_data table: {e}")
     
 def populate_table(conn, filename, table):
     cursor = conn.cursor()
@@ -88,12 +224,15 @@ def greater_equal_condition(field1, field2):
 def greater_condition(field1, field2):
     return f"{field1}>{field2}"
     
-def query_table(conn, table, fields=['*'], conditions=[]):
-    cursor = conn.cursor()
-    cursor.execute(f"""
-                   SELECT {', '.join(fields)} FROM {table} WHERE {'AND '.join(conditions)}; 
-                   """)
-    conn.commit()
-    result = cursor.fetchall()
-    cursor.close()
-    return result
+def query_table(conn, table, fields=['*'], conditions=[], limit=10):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(f"""
+                    SELECT {', '.join(fields)} FROM {table} WHERE {'AND '.join(conditions)} LIMIT {limit}; 
+                    """)
+        conn.commit()
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+    except Exception as e:
+        print(f"The following error occured in the query to {table}: {e}")
